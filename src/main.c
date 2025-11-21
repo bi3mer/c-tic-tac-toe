e9e790a59f8340da4b7b2c51d2d9aa81ced9f284
bi3mer <colan@comcast.net>
2025-11-21 10:40:47 -0500

    scenes

--- src/main.c
+++ src/main.c
@@ -1,19 +1,65 @@
+#include "assert.h"
 #include "raylib.h"
+#include <stdio.h>
+
+typedef enum
+{
+    Scene_Menu,
+    Scene_Game,
+} Scene;
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screen_width = 800;
+    const int screen_height = 800;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Template");
+    Scene scene = Scene_Menu;
+
+    InitWindow(screen_width, screen_height, "Raylib Template");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+        switch (scene)
+        {
+        case Scene_Menu:
+        {
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                scene = Scene_Game;
+            }
+            break;
+        }
+        case Scene_Game:
+        {
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                scene = Scene_Menu;
+            }
+            break;
+        }
+        default:
+            assert(false);
+        }
 
-        DrawText("TEXT HERE", 190, 200, 20, BLACK);
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        switch (scene)
+        {
+        case Scene_Menu:
+        {
+            DrawText("Menu Scene", 100, 100, 40, WHITE);
+            break;
+        }
+        case Scene_Game:
+        {
+            DrawText("Game Scene", 100, 100, 40, WHITE);
+            break;
+        }
+        default:
+            assert(false);
+        }
 
         EndDrawing();
     }
